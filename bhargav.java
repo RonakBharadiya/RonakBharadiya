21733794cf12e7a12d9792a9226ff59be4612685
bhargav <bharadiyabhargav4549@gmail.com>
2021-09-13 17:08:14 +0530

    my name is bhargav

---
+++ bhargav.java
@@ -0,0 +1,12 @@
+public class bhargav{
+    public static void main(String[]args)
+    {
+        int a=2,b=5,c=27;
+                System.out.println("sum of a and b is"+(a+b));
+
+
+
+
+    }
+
+}
